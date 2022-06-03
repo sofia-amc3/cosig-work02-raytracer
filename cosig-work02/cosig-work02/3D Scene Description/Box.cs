bd4512a0a6f0c0dc521733f332f791817b9d096f
Sofia Canelas <1200185@isep.ipp.pt>
2022-06-03 14:34:01 +0100

    Updated Box Intersection Method (not finished)

--- cosig-work02/cosig-work02/3D Scene Description/Box.cs
+++ cosig-work02/cosig-work02/3D Scene Description/Box.cs
@@ -12,28 +12,53 @@ namespace cosig_work02
 
         public override bool intersect(Ray ray, Hit hit)
         {
-            // calculate intersection distance t1 and t2
-            Vector3 origin = ray.getOrigin(), // R
-                    direction = ray.getDirection(), // D
-                    x1 = new Vector3(0, 0, 0),
-                    x2 = new Vector3(1, 1, 1),
-                    invertedDirection = new Vector3(1 / direction.getX(), 1 / direction.getY(), 1 / direction.getZ()),
-                    t1 = Vector3.multiplyVectorByVector(Vector3.subtractVectors(x1, origin), invertedDirection),
-                    t2 = Vector3.multiplyVectorByVector(Vector3.subtractVectors(x2, origin), invertedDirection);
+            // general
+            Vector3 center = new Vector3(0, 0, 0), // box is centered at origin
+                    origin = ray.getOrigin(),
+                    direction = ray.getDirection();
+            double width = 1,  // box x
+                   height = 1, // box y
+                   length = 1; // box z
 
-            double t1Length = Vector3.calculateVectorLength(t1),
-                   t2Length = Vector3.calculateVectorLength(t2);
+            // x axis ------------------------------------------------------------
+            double x1 = center.getX() - width / 2,
+                   x2 = center.getX() + width / 2,
+                   t1x = (x1 - origin.getX()) / direction.getX(),
+                   t2x = (x2 - origin.getX()) / direction.getX();
 
-            if(t1Length > t2Length) // if t1 > t2, swap
+            if(t1x > t2x) // if t1 > t2, swap
             {
-                Vector3 temp = t1;
-                t1 = t2;
-                t2 = temp;
+                double tempx = t1x;
+                t1x = t2x;
+                t2x = tempx;
             }
 
-            // if tnear > tfar, box is missed
-            // if tfar < 0, box is behind
-            // if box survived tests, report intersection at tnear
+            // y axis ------------------------------------------------------------
+            double y1 = center.getY() - height / 2,
+                   y2 = center.getY() + height / 2,
+                   t1y = (y1 - origin.getY()) / direction.getY(),
+                   t2y = (y2 - origin.getY()) / direction.getY();
+
+            if (t1y > t2y) // if t1 > t2, swap
+            {
+                double tempy = t1y;
+                t1y = t2y;
+                t2y = tempy;
+            }
+
+            // z axis ------------------------------------------------------------
+            double z1 = center.getZ() - length / 2,
+                   z2 = center.getZ() + length / 2,
+                   t1z = (z1 - origin.getZ()) / direction.getZ(),
+                   t2z = (z2 - origin.getZ()) / direction.getZ();
+
+            if (t1z > t2z) // if t1 > t2, swap
+            {
+                double tempz = t1z;
+                t1z = t2z;
+                t2z = tempz;
+            }
+
             return false;
         }
     }
